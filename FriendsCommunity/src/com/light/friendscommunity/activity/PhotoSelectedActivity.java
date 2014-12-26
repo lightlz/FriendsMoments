d7a2f147411fc4b0ae87c1e526fe5e97963fe216
light <lailiangzhong@long.tv>
2014-12-26 14:06:26 +0800

    add albumNameSelected PopupWindow

--- FriendsCommunity/src/com/light/friendscommunity/activity/PhotoSelectedActivity.java
+++ FriendsCommunity/src/com/light/friendscommunity/activity/PhotoSelectedActivity.java
@@ -1,21 +1,75 @@
 package com.light.friendscommunity.activity;
 
 import com.light.friendscommunity.R;
+import com.light.friendscommunity.utils.ThumbnailUtil;
+import com.light.friendscommunity.widget.ViewHolder;
 
+import android.R.color;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.renderscript.Sampler;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
-public class PhotoSelectedActivity extends Activity{
+/**
+ * 
+* @ClassName PhotoSelectedActivity 
+* @Description TODO(照片选择) 
+* @date 2014年12月26日
+*
+ */
+public class PhotoSelectedActivity extends Activity implements OnClickListener{
 	
 	private ActionBar actionBar;
 	
 	private static final String TAG = "PublishActivity : ";
+	
+	private static final int DATATHREAD_FINISH = 0;
 
+	private LinearLayout ablumSelectLayout;
+	
+	private RelativeLayout layout;
+	
+	private ThumbnailUtil thumbUtil;
+	
+	private TextView ablumNameTv;
+	
+	private Handler handler = new Handler(){
+
+		@Override
+		public void handleMessage(Message msg) {
+			// TODO Auto-generated method stub
+			super.handleMessage(msg);
+			
+			switch(msg.what){
+			case DATATHREAD_FINISH:
+				ablumNameTv.setText(thumbUtil.list.get(0).getAlbumName()+"");
+				break;
+			}
+		}
+		
+		
+	};
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -31,6 +85,15 @@ public class PhotoSelectedActivity extends Activity{
 		actionBar.setTitle("Gallery");
 		actionBar.setDisplayShowHomeEnabled(false);
 		
+		layout = (RelativeLayout)findViewById(R.id.relativeLayout);
+		
+		ablumSelectLayout = (LinearLayout)findViewById(R.id.layout_photosele_album);
+		ablumSelectLayout.setOnClickListener(this);
+		
+		ablumNameTv = (TextView)findViewById(R.id.tv_photosele_album);
+		
+		
+		new Thread(new GetDataThread()).start();
 	}
 
 	@Override
@@ -49,9 +112,6 @@ public class PhotoSelectedActivity extends Activity{
 		int id = item.getItemId();
 		switch(id){
 		  case android.R.id.home:
-//	            Intent intent = new Intent(this, PublishActivity.class);
-//	            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-//	            startActivity(intent);
 			    finish();
 	            return true;
 	        default:
@@ -66,5 +126,100 @@ public class PhotoSelectedActivity extends Activity{
 		super.onDestroy();
 	}
 
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		int id = v.getId();
+		switch(id){
+		case R.id.layout_photosele_album:
+			showAlbumPopu();
+			break;
+		}
+	}
+
+	/**
+	 * 
+	* @Description TODO(显示相册选择的popup) 
+	* @param    
+	* @return void  
+	 */
+	private void showAlbumPopu(){
+
+	     View contentView = getLayoutInflater().inflate(R.layout.item_popu_album_seleted, null); 
+	        
+		 int h = getWindowManager().getDefaultDisplay().getHeight();  
+	     int w = getWindowManager().getDefaultDisplay().getWidth(); 
+	     
+		 PopupWindow pw = new PopupWindow(contentView);
+		 pw.setHeight(h/3);
+		 pw.setWidth(w);
+		 pw.setOutsideTouchable(false);
+		 pw.setFocusable(true);
+		 pw.setBackgroundDrawable(new BitmapDrawable()); 
+		 
+		 ListView lv = (ListView)contentView.findViewById(R.id.lv_ablum_seleted);
+		 ListViewAdapter adapter = new ListViewAdapter();
+		 lv.setAdapter(adapter);
+		 
+		 pw.showAtLocation(layout, Gravity.BOTTOM, 0,ablumSelectLayout.getHeight());
+	}
 	
+	
+	class GetDataThread extends Thread{
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			super.run();
+			thumbUtil = new ThumbnailUtil(PhotoSelectedActivity.this);
+			if(!(thumbUtil.hash.size()>0 && thumbUtil.list.size()>0)){
+				thumbUtil.getThumbnailPath();
+				thumbUtil.getOriginImagePath();
+			}
+			handler.sendEmptyMessage(DATATHREAD_FINISH);
+		}
+		
+	}
+	
+	class ListViewAdapter extends BaseAdapter{
+
+		
+		@Override
+		public int getCount() {
+			// TODO Auto-generated method stub
+			return thumbUtil.list.size();
+		}
+
+		@Override
+		public Object getItem(int position) {
+			// TODO Auto-generated method stub
+			return thumbUtil.list.get(position);
+		}
+
+		@Override
+		public long getItemId(int position) {
+			// TODO Auto-generated method stub
+			return position;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			// TODO Auto-generated method stub
+			if(convertView==null){
+				convertView = LayoutInflater.from(PhotoSelectedActivity.this)
+						.inflate(R.layout.item_lv_popu_ablum, null);
+			}
+			String ablumsName = thumbUtil.list.get(position).getAlbumName();
+			int imageSum = thumbUtil.list.get(position).getList().size();
+			
+			TextView nameTv = ViewHolder.get(convertView,R.id.tv_lv_popu_sele);
+			nameTv.setText(ablumsName);
+			
+			TextView sumTv = ViewHolder.get(convertView, R.id.tv_lv_popu_sum);
+			sumTv.setText(String.valueOf(imageSum));
+			
+			return convertView;
+		}
+		
+	}
 }
--- FriendsCommunity/src/com/light/friendscommunity/utils/ThumbnailUtil.java
+++ FriendsCommunity/src/com/light/friendscommunity/utils/ThumbnailUtil.java
@@ -9,6 +9,7 @@ import com.light.friendscommunity.bean.AlbumsBean;
 import com.light.friendscommunity.bean.ThumbnailImageBean;
 
 import android.content.ContentResolver;
+import android.content.Context;
 import android.database.Cursor;
 import android.provider.MediaStore;
 import android.provider.MediaStore.Images.Thumbnails;
@@ -19,17 +20,30 @@ public class ThumbnailUtil {
 	private ContentResolver cr; 
 	
 	/** 保存缩略图的path，key为imageId */
-	private static HashMap<Integer,String> hash = new HashMap<Integer, String>();
+	public  static HashMap<Integer,String> hash = new HashMap<Integer, String>();
+	
+	private Context context;
 	
 	/**保存相簿列表*/
-	private List<AlbumsBean> list = new ArrayList<AlbumsBean>();
+	public static List<AlbumsBean> list = new ArrayList<AlbumsBean>();
+	
+
+	public ThumbnailUtil(Context context) {
+		super();
+		this.context = context;
+		
+		cr = context.getContentResolver();
+		
+	}
+
+
 	/**
 	 * 
 	* @Description TODO(获取缩略图的路径，并保存) 
 	* @param    
 	* @return void  
 	 */
-	private void getThumbnailPath(){
+	public void getThumbnailPath(){
 		
 		String[] thumbnailsInfo = { Thumbnails._ID, Thumbnails.IMAGE_ID, Thumbnails.DATA };
 		Cursor cur = cr.query(Thumbnails.EXTERNAL_CONTENT_URI, thumbnailsInfo, null, null, null);
@@ -54,12 +68,14 @@ public class ThumbnailUtil {
 	* @param    
 	* @return void  
 	 */
-	private void getOriginImagePath(){
+	public List<AlbumsBean> getOriginImagePath(){
+		
 		
 		final String id = "_id";
 		final String dataPath = "_data";
 		final String albumName = "bucket_display_name";
 		final String limit = "date_modified DESC";
+		
 		
 		Cursor cursor = cr.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, null, null, null, limit);  
 		/**保存相簿 key为相簿名*/
@@ -112,7 +128,8 @@ public class ThumbnailUtil {
 				
 			}while (cursor.moveToNext());
 		}
-
+		return list;
 	}
 
+	
 }
